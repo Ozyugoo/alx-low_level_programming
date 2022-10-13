fa1e54925fd528392fb86df05a929920af0e1d7d
Ugoo-Okonkwo Stella <stellaozy22@gmail.com>
2022-10-13 14:45:44 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ *main -output
+ *Return: Always 0
+ */
 int main() {
-	puts("programming is like building a multilingual puzzle,\n");
+	puts("\"programming is like building a multilingual puzzle,\n");
 	Return (0);
 }	
 
