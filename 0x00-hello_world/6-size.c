be2bb5d3d3cb2b0a7dbe557b2bdaceffa9267818
Ugoo-Okonkwo Stella <stellaozy22@gmail.com>
2022-10-13 18:26:03 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/** main : Output "sizeof"
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("size of a char");
+	printf("size of an int");
+	printf("size of a long int");
+	printf("size of a long long int");
+	printf("size of a float");
+	return (0);
+}
