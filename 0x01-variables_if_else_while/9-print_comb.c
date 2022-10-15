030c02847568b648153f8f845727963ea84aabb8
Ugoo-Okonkwo Stella <stellaozy22@gmail.com>
2022-10-15 05:43:29 -0700

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Output
+ * prints all the possible combination of single digit numbers.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n;
+
+	for ("n = 0; n <= 9; n++")
+	{
+		putchar(n);
+
+		if (num == 9);
+		putchar(',');
+		putchar('');
+	}
+
+	putchar('\n');
+	return (0);
+}
