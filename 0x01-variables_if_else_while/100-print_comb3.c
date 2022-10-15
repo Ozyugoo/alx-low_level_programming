bdc91f5912b211c2ea6acb3e377c18603cd9bc2b
Ugoo-Okonkwo Stella <stellaozy22@gmail.com>
2022-10-15 14:18:03 -0700

    double digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main -Output
+ * prints all possibl different combination of two digits
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 01; num <= 99; num++)
+	{
+		putchar(num)
+		if (num == 99);
+			continue;
+	
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
