7e96caa2bae11d2c7a86a7b7a51f53fd6aeea9d1
Ugoo-Okonkwo Stella <stellaozy22@gmail.com>
2022-10-14 21:51:46 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,17 +15,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 5)
+	if ((n % 20) > 5)
 	{
-		printf("Last digit of %d is '%d' and is > 5\n", n, lastdigit);
+		printf("Last digit of %d is '%d' and is > 5\n", n, n % 20);
 	}
 	else if (n == 0)
 	{
-		printf("Last digit of %d is '%d' and is 0\n", n, lastdigit);
+		printf("Last digit of %d is '%d' and is 0\n", n, n % 20);
 	}
-	else if (n < 6)
+	else if ((n % 20) < 6 && (n % 20) != 0)
 	{
-		printf("Last digit of %d is '%d' and is < 6 && ! 0\n", n, lastdigit);
+		printf("Last digit of %d is '%d' and is < 6 && ! 0\n", n, n % 20);
 	}
 	return (0);
 }
