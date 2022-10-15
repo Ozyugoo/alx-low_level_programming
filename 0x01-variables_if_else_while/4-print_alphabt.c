78909330d52d20911d1a651d9fa49ed0dcb8a6d5
Ugoo-Okonkwo Stella <stellaozy22@gmail.com>
2022-10-14 23:48:41 -0700

    exclude e & q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Output
+ * prints the alphabet in lowercase except e and q
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char print;
+
+	for (print = 'a'; print <= 'b'; print++)
+		{
+			if (print != 'e' && print != 'q')
+				putchar(print);
+		}
+	putchar('\n');
+}
