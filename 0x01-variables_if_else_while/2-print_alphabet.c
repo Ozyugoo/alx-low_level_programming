bb7cc0fbe6daf4c02c7c1496a47ed312b4a6e2ce
Ugoo-Okonkwo Stella <stellaozy22@gmail.com>
2022-10-14 22:25:08 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Output
+ * prints all alphabets in lowercase
+ *
+ * Return : Always 0
+ */
+
+int main(void)
+
+{
+	for (write ='a'; write <='z'; write++)
+
+		putchar (write);
+
+		putchar ('\n')
+
+	return: 0
+
+}
