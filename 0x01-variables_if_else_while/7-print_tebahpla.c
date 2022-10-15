e69963a0fb11b673e7638830256afe99be201301
Ugoo-Okonkwo Stella <stellaozy22@gmail.com>
2022-10-15 05:00:14 -0700

    lowercase disorder

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Output
+ * prints the lowercase alphabet in reverse disorder
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char print;
+
+	for (print = 'z'; print <= 'a'; print++)
+		putchar(print);
+
+	putchar('\n');
+
+	return (0);
+}
