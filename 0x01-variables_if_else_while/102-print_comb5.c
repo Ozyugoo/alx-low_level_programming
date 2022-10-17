8f4cc8b916d94ee97316afcbcee39b499d46a344
Ugoo-Okonkwo Stella <stellaozy22@gmail.com>
2022-10-17 02:28:55 -0700

    two two-digit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,28 +9,24 @@
 
 int main(void)
 {
-	int num1, num2, num3, num4;
+	int num1, num2;
 	{
-		for (num1 = 0; num1 < 9; num1++)
+		for (num1 = 0; num1 <= 99; num1++)
 		{
-			for (num2 = num1 + 1; num2 < 10; num2++)
+			for (num2 = num1 + 1; num2 < 100; num2++)
 			{
-				for (num3 = num2 + 1; num3 < 99; num3++)
-				{
-					for (num4 = num3 + 1; num4 < 100; num4++)
-					{
-						putchar((num1 % 10) + '0');
-						putchar((num2 % 10) + '0');
-						putchar(' ');
-						putchar((num3 % 100) + '0');
-						putchar((num4 % 100) + '0');
+				putchar((num1 / 10) + '0');
+				putchar((num1 % 10) + '0');
+				putchar(' ');
+				putchar((num2 / 10) + '0');
+				putchar((num2 % 10) + '0');
 
-						if (num1 == 8 && num2 == 9 && num3 == 98 && num4 == 99)
-							continue;
-					}
-						putchar(',');
-						putchar(' ');
-				}
+				if (num1 == 98 && num2 == 99)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+
 			}
 		}
 	}
