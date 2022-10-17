df48062404860034113e7b3869199d3c442a940b
Ugoo-Okonkwo Stella <stellaozy22@gmail.com>
2022-10-16 23:21:43 -0700

    three digit combo

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 
 /**
- * main -Output
+ * main - Output
  * prints all possible different combinations of three digits.
  *
- * Returns: Always 0
+ * Returns: Always 0.
  */
 
 int main(void)
 {
 	int num1, num2, num3;
 
-	for (num1 = 0; num1 < 9; num1++)
+	for (num1 = 0; num1 < 8; num1++)
 	{
-	
-		for (num2 = num1 + 1; num2 < 10; num2++)
+		for (num2 = num1 + 1; num2 < 9; num2++)
 		{	
-		
-			for (num3 = num1 + 2; num3 < 10; num3++)
+			for (num3 = num2 + 1; num3 < 10; num3++)
 			{
 				putchar((num1 % 10) + '0');
 				putchar((num2 % 10) + '0');
@@ -25,7 +23,7 @@ int main(void)
 
 				if (num1 == 7 && num2 == 8 && num3 == 9)
 					continue;
-		
+	
 				putchar(',');
 				putchar(' ');
 			}
