6d2dd07e84cd1783dc7c7cfd5c5f9369374a46f0
Ugoo-Okonkwo Stella <stellaozy22@gmail.com>
2022-10-22 10:24:35 -0700

    0main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Output positive or negative
+ * tests if function prints the correct output when tested with 0
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
