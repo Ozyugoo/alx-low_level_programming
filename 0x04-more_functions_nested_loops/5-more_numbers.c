44e1084e6563deb169c09b4a8bddf8cbf231ed61
Ugoo-Okonkwo Stella <stellaozy22@gmail.com>
2022-10-23 05:19:28 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - Output
+ * prints numbers 1-14 in multiples of 10
+ */
+
+void more_numbers(void)
+{
+	int n, int p;
+
+	for (p = 0; p <= 9; p++)
+	{
+		for (n =0; n <= 14; n++)
+		{
+			if (p > 9)
+				_putchar((p / 10) +'0');
+			_putchar((p % 10) +'0');
+		}
+		_putchar('\n');
+	}
+}
