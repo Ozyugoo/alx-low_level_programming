7995c96283e0293d5ee6ae92d0f1baa96dc99cd8
Ugoo-Okonkwo Stella <stellaozy22@gmail.com>
2022-10-28 10:46:36 -0700

    strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,19 +17,19 @@ char *cap_string(char *str)
 
 		if (str[index - 1] == ' ' ||
 		    str[index - 1] == '\t' ||
-    		    str[index - 1] == '\n' ||
-   		    str[index - 1] == ',' ||
-    		    str[index - 1] == ';' ||
-    		    str[index - 1] == '.' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
 		    str[index - 1] == '!' ||
-    		    str[index - 1] == '?' ||
-   		    str[index - 1] == '"' ||
-    		    str[index - 1] == '(' ||
-    		    str[index - 1] == ')' ||
-    		    str[index - 1] == '{' ||
-    		    str[index - 1] == '}' ||
-    		    index == 0)
-    			str[index] -= 32;
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
 
 		index++;
 	}
