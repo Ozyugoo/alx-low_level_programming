8f80756f98b76d6738f6110d1a1619bc0d8ada3c
Ugoo-Okonkwo Stella <stellaozy22@gmail.com>
2022-10-30 14:28:44 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: first integer to be added
+ * @n: second integer to be added
+ * Returns: returns sum of a and n
+ */
+int add(int a, int n)
+{
+int c;
+
+c = a + n;
+
+return (c);
+}
