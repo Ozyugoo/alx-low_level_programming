25700ec302917197f3fb58227c8657e98e910647
Ugoo-Okonkwo Stella <stellaozy22@gmail.com>
2022-11-03 11:07:34 -0700

    --amend

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - prints the value of x raised to the power y
+ * @x: the value to be raised
+ * @y: the value raised
+ * Return: the value of xraised to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+_pow_recursion(x, y);
+if (y < 0)
+return (-1);
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _sqrt_recursion - prints the square root of natural numbers
+ * @n: the number to find its square root
+ * Return: the square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+sqrt(n);
+if (n != sqrt)
+return (-1);
+}
